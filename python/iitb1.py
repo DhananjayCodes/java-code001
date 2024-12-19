dfd0289b8ab1c86c1b4458dc88d026608086d847
Dhananjay Rawat <dhananjay28it024@satiengg.in>
2024-12-19 15:13:43 +0530

    update

---
+++ python/iitb1.py
@@ -0,0 +1,12 @@
+# n=1000
+# result=0
+# for x in range(1,n):
+#     for y in range(1,n):
+#         for z in range(1,n):
+#             if(x*x+y*y==z*z):
+#                 if x*y*z==1000:
+#                     result=x*y*z
+
+# print("hello")
+# print(result)
+print("hello")
